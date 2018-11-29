c6f02339263574962677b42c830c6b8d51d1bd15
Pedro Oliveira <pjmc.oliveira@gmail.com>
2018-11-29 17:38:16 -0500

    Created skeleton with client, clone, data

---
+++ src/client.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// required for reading files
+const fs = require('fs');
+
+// required for connecting to GraphQL API
+const graphqlClient = require('graphql-client');
+
+// a simple wrapper that initializes our GraphQL client with the auth token
+class Client {
+    constructor(givenToken) {
+        try {
+            this.token = givenToken || fs.readFileSync('./auth_token.txt', 'utf8');
+        } catch (err) {
+            // catch common causes of errors
+            console.log('An error occurred loading the authentication token!');
+            console.log('Verify you have the \'auth_token.txt\' file saved on this directory.');
+            console.log('Details');
+            throw err;
+        }
+        
+        // initialize GraphQL client
+        // See [https://github.com/nordsimon/graphql-client#readme] for details
+        this.client = graphqlClient({
+            url: 'https://api.github.com/graphql',
+            headers: {
+                Authorization: 'bearer ' + this._token
+            }
+        });
+    }
+
+    // simple wrapper for most common tasks
+    // client available through ._client directly
+    query(Q, vars) {
+        return this._client.query(Q, vars);
+    }
+}
+
+module.exports = {
+    Client: Client
+};
---
+++ src/clone.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const tmp = require('tmp');
+const Git = require('nodegit');
+
+class Clone {
+    static async init(url) {
+        // create tmp directory
+        const path = tmp.dirSync().name;
+        const repo = await Git.Clone(url, path);
+        return new Clone(path, repo);
+    }
+
+    constructor(path, repo) {
+        if (path === undefined || repo === undefined) {
+            throw Error('Cannot be called directly!');
+        }
+        this.path = path;
+        this.repo = repo;
+    }
+
+}
+
+module.exports = {
+    Clone: Clone
+};
