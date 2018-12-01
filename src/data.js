ceee8f84fd6a79b16bd24a71269a69a742a3b910
Pedro Oliveira <pjmc.oliveira@gmail.com>
2018-11-30 21:31:31 -0500

    added getIssuesInfo method

--- src/data.js
+++ src/data.js
@@ -29,10 +29,9 @@ module.exports.Data = class Data {
      *      The number of issues
      */
     getNumberOfIssues() {
-        const vars = {name: this.repoName, owner: this.owner};
         const query = `
-            query repo($name: String!, $owner: String!){
-                repository(name: $name, owner: $owner){
+            query repo{
+                repository(name: "${this.repoName}", owner: "${this.owner}"){
                     total: issues {
                         totalCount
                     }
@@ -44,14 +43,85 @@ module.exports.Data = class Data {
                     }
                 }
             }`;
-        return this.client.query(query, vars).then(body => {
-            return body.data.repository
-        }).then(repo => {
-            return {
+        return this.client.query(query, {}).then(
+            body => body.data.repository
+        ).then(
+            repo => ({
                 total: repo.total.totalCount,
                 open: repo.open.totalCount,
                 closed: repo.closed.totalCount,
-            };
-        });
+            })
+        );
+    }
+
+    /**
+     *  Get details from the first N issues of each kind (any/open/closed)
+     *  @param({
+     *      total:  [number],
+     *      open:   [number],
+     *      closed: [number],
+     *  })
+     *      The first N issues to get by kind
+     *
+     *  @return {Promise({
+     *      total: {state: [string], createdAt: [Date]},
+     *      open: {createdAt: [Date]},
+     *      closed: {createdAt: [Date], closedAt: [Date]}
+     *  })}
+     *      The info for the issues
+     */
+    getIssuesInfo({total, open, closed} = {}) {
+        // TODO: better guards or optional args
+        const assert = require('assert');
+        assert(total !== undefined, 'total must be defined!');
+        assert(open !== undefined, 'open must be defined!');
+        assert(closed !== undefined, 'closed must be defined!');
+
+        const query = `
+            query repo{
+                repository(name: "${this.repoName}", owner: "${this.owner}"){
+                    total: issues (
+                            first: ${total},
+                            orderBy: {field:CREATED_AT, direction:ASC}) {
+                        nodes {
+                            state
+                            createdAt
+                        }
+                    }
+                    open: issues (
+                            states: [OPEN],
+                            first: ${open},
+                            orderBy: {field:CREATED_AT, direction:ASC}) {
+                        nodes {
+                            createdAt
+                        }
+                    }
+                    closed: issues (
+                            states: [CLOSED],
+                            first: ${closed},
+                            orderBy: {field:CREATED_AT, direction:ASC}) {
+                        nodes {
+                            createdAt
+                            closedAt
+                        }
+                    }
+                }
+            }`;
+        return this.client.query(query, {}).then(
+            body => body.data.repository
+        ).then(repo => ({
+            total: repo.total.nodes.map(n => ({
+                state: n.state,
+                createdAt: Date.parse(n.createdAt)
+            })),
+            open: repo.open.nodes.map(n => ({
+                createdAt: Date.parse(n.createdAt)
+            })),
+            closed: repo.closed.nodes.map(n => ({
+                createdAt: Date.parse(n.createdAt),
+                closedAt: Date.parse(n.closedAt)
+            }))
+        }))
+
     }
 };
