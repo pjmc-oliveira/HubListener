68e73a8a344c547489e97ed057692a00b3703f4c
Pedro Oliveira <pjmc.oliveira@gmail.com>
2019-01-11 14:16:27 -0500

    refactored app.js into a main function. Added command line options (for url, no-clone, help)

--- src/app.js
+++ src/app.js
@@ -2,24 +2,46 @@
     app.js serves as the main entry point for Hublistener.
  */
 
-const fs = require ('fs');
 const utils = require('./utils');
-const {Data} = require ('./data');
+const { Data } = require ('./data');
 
-// Parse arguments
-// const repo_url = process.argv[2];
-// const vars = utils.parseURL(repo_url);
+function main(args) {
+    // Parse arguments
+    const options = utils.argParse(args);
 
-// Get auth token
-// TODO: add option to provide path to auth token file or auth token string as an argument
+    // Available options and flags
+    // NOTE: Update options message whenever a new option is added or removed
+    // TODO: add option to provide path to auth token file or auth token string as an argument
+    const optionsMsg = `
+    Usage:  node app.js [--url <url>] [options...]
 
+    -h, --help  : print command line options
+    --url <url> : GitHub project url
+    --no-clone  : Don't clone repository
 
+    Documentation can be found at:
+    https://github.com/pjmc-oliveira/HubListener
+    `;
 
-const args = utils.argParse(process.argv)
-console.log(args);
+    // If help flag, display options and exit
+    if (options['h'] || options['help']) {
+        console.log(optionsMsg);
+        return;
+    }
 
-// Create Data object
-// var data = new Data(repo_url);
-// data.clonePromise.then(function (clone) {
-//     console.log(clone.path);
-// });
+    const repoUrl = options['url'];
+    // If no repository url was provided, display options and exit
+    if (repoUrl === undefined) {
+        console.log(optionsMsg);
+        return;
+    }
+
+    // Create new Data object
+    const data = new Data(repoUrl, {noClone: options['no-clone']});
+
+    data.getNumberOfCommitsByTime().then(x => {
+        console.log(x)
+    });
+}
+
+main(process.argv);
