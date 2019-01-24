de2af4bcdc3617c119fa2504776ea5fa5ddb9bb9
Pedro Oliveira <pjmc.oliveira@gmail.com>
2019-01-24 16:04:03 -0500

    Added JSDoc to Clone class

--- src/clone.js
+++ src/clone.js
@@ -1,13 +1,20 @@
 'use strict';
 
+// node and npm modules
 const tmp = require('tmp');
 const Git = require('nodegit');
 
 
 /**
- *  @class The Clone class
+ *  @class The Clone class to clone and manage the Git repository
  */
-module.exports.Clone = class Clone {
+class Clone {
+    /**
+     *  Initialize and return an instance of the {@link Clone} class.
+     *  @param {string} url - A valid URL to a Git repository.
+     *
+     *  @return {Promise<Clone>} A promise to a {@link Clone} instance.
+     */
     static async init(url) {
         // create tmp directory
         const path = tmp.dirSync().name;
@@ -15,6 +22,13 @@ module.exports.Clone = class Clone {
         return new Clone(path, repo);
     }
 
+    /**
+     *  Constructs a {@link Clone} object.
+     *  <br>WARNING: Not to be instantiated directly, see [init]{@link Clone.init}
+     *  @param {string} path - Path to the repository location on drive
+     *  @param {Git.Clone} repo
+     *      A [Git.Clone]{@link https://www.nodegit.org/api/clone/} instance
+     */
     constructor(path, repo) {
         if (path === undefined || repo === undefined) {
             throw Error('Cannot be called directly!');
@@ -23,4 +37,8 @@ module.exports.Clone = class Clone {
         this.repo = repo;
     }
 
+}
+
+module.exports = {
+    Clone: Clone
 };
