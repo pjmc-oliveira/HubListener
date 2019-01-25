b0d97d4a6a1672b3917fdf3247317838a5677328
Pedro Oliveira <pjmc.oliveira@gmail.com>
2019-01-24 21:06:45 -0500

    Added JSDoc to client.js

--- src/client.js
+++ src/client.js
@@ -1,18 +1,32 @@
 'use strict';
 
-// required for reading files
+// node and npm modules
 const fs = require('fs');
-
-// required for connecting to GraphQL API
 const graphqlClient = require('graphql-client');
 
-// a simple wrapper that initializes our GraphQL client with the auth token
 /**
- *  @class The Client class
+ *  @class The Client class is a simple wrapper that initializes our
+ *  [GraphQL]{@link https://graphql.org} client with the auth token. Current
+ *  implementation uses
+ *  [graphql-client]{@link https://github.com/nordsimon/graphql-client#readme}.
  */
-module.exports.Client = class Client {
+class Client {
+    /**
+     *  Constructs a {@link Client} object.
+     *  @param {string} [givenToken]
+     *      A [GitHub authentication token]{@link https://github.com/settings/tokens}
+     *      to connect to the [GitHub API]{@link https://developer.github.com/v4/}.
+     *      The token needs `public_repo` access. If a token is not provided,
+     *      the program will attempt to read from a named file 'auth_token.txt'
+     *      on the `src` directory. If none exists the constructor will crash.
+     */
     constructor(givenToken) {
         try {
+            /**
+             *  The API token
+             *  @name Client#token
+             *  @type {string}
+             */
             this.token = givenToken || fs.readFileSync('./auth_token.txt', 'utf8');
         } catch (err) {
             // catch common causes of errors
@@ -22,8 +36,13 @@ module.exports.Client = class Client {
             throw err;
         }
         
+        /**
+         *  The API client, see
+         *  [documentation]{@link https://github.com/nordsimon/graphql-client#readme}
+         *  @name Client#client
+         *  @type {graphql-client}
+         */
         // initialize GraphQL client
-        // See [https://github.com/nordsimon/graphql-client#readme] for details
         this.client = graphqlClient({
             url: 'https://api.github.com/graphql',
             headers: {
@@ -32,9 +51,17 @@ module.exports.Client = class Client {
         });
     }
 
-    // simple wrapper for most common tasks
-    // client available through ._client directly
+    /**
+     *  A simple wrapper for most common tasks client available through
+     *  Client#client directly.
+     *  @param {string} Q - The [GraphQL]{@link https://graphql.org} query.
+     *  @param {object} vars - The variables to be replaced in the query.
+     */
     query(Q, vars) {
         return this.client.query(Q, vars);
     }
+}
+
+module.exports = {
+    Client: Client
 };
