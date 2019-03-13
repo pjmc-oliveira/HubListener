031c1bb0e963ad77f62ad2042ed43b362ef63023
Pedro Oliveira <pjmc.oliveira@gmail.com>
2019-03-13 09:28:02 -0400

    Began work on /analyse api endpoint. Currently gets or inserts repository info to database

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+
+const { Data } = require('./data.js');
+const { makeDB } = require('./database.js');
+const utils = require('./utils.js');
+
 const app = express();
 const port = 8080;
-const {Data} = require('./data');
+// promise to a database wrapper
+const _db = makeDB('hubdata.sqlite3');
 
 app.use(express.static('static'));
 app.use(express.json());
@@ -41,6 +47,39 @@ app.post('/run', (req, res) => {
 
         res.send(results);
     });
+});
+
+app.post('/analyse', async (req, res) => {
+    // wait for our db to load
+    const db = await _db;
+
+    // parse url from body
+    const { url, options } = req.body;
+    const { owner, name } = utils.parseURL(url);
+
+    // ensure repo is in database
+    const repoId = db.get.repo({owner, name})
+        // if repo not present, insert it
+        .then(repo => repo ?
+            repo :
+            db.insert.repo({owner, name}))
+        .then(rowOrInfo => rowOrInfo.id !== undefined ?
+            // return the id of the already present repo
+            rowOrInfo.id :
+            // or the last id of the table after it's inserted
+            rowOrInfo.lastID);
+
+    // clone and update repo
+    const data = new Data(url, options);
+
+    // wait for both
+    const results = Promise.all([data.clonePromise, repoId]).then(async ([clone, info]) => {
+        console.log('in promise...');
+        console.log(info);
+        return {};
+    });
+
+    res.send({data: await results});
 });
 
 app.listen(port, () => console.log(`listening on ${port}`));
