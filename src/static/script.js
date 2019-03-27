52bcb833d333239857bbc19e4526e32ad8e431c3
Zed Ahmad <ahmadza@mcmaster.ca>
2019-03-27 15:03:05 -0400

    [ZA] Implemented chart controls.

--- src/static/script.js
+++ src/static/script.js
@@ -1,16 +1,17 @@
+let chart;
+let chartData;
+let chartConfig;
+
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('form');
     form.addEventListener('submit', submitForm);
 });
 
 function ajax(method, url, json) {
-    console.log('making promise');
     return new Promise((resolve, reject) => {
-        console.log('in promise')
         const xhr = new XMLHttpRequest();
         xhr.open(method, url, true);
         xhr.onload = () => {
-            console.log('in onload')
             if (xhr.status >= 200 && xhr.status < 300) {
                 resolve(JSON.parse(xhr.response));
             } else {
@@ -31,6 +32,12 @@ function form2json(form) {
     return result;
 }
 
+function updateChart() {
+    // Need to reinstantiate the quick filter plugin due to a bug in taucharts updateConfig implementation
+    chartConfig.plugins[0] = Taucharts.api.plugins.get('quick-filter')(['commit_date']);
+    chart.updateConfig(chartConfig);
+}
+
 function submitForm(event) {
     event.preventDefault();
     $("#form").hide();
@@ -38,33 +45,59 @@ function submitForm(event) {
     const json = form2json(event.target);
     ajax("POST", "/analyse", json)
         .then(x => {
-            console.log(x); // dump results to log
-
-            let data = x.points;
-
-            for (d of data) {
-                d['commit_date'] = new Date(d['commit_date']);
+            // Make any necessary transformations to the parsed JSON results
+            for (data of x.points) {
+                // Parse dates into date objects
+                data['commit_date'] = new Date(data['commit_date']);
             }
+
+            chartData = x.points;
 
             $("#loading").hide();
             $("#results").show();
 
-            new Taucharts.Chart({
-                data: data,
+            // First time / default config
+            chartConfig = {
+                data: chartData,
                 type: 'line',
                 x: 'commit_date',
                 y: 'numberOfFiles',
                 color: 'file_extension',
                 guide: {
-                    x: {nice: false},
-                    y: {nice: true},
-                    padding: {b:40,l:40,t:10,r:10}
-                },
+                x: {nice: true},
+                y: {nice: true},
+                padding: {b:40,l:40,t:10,r:10}
+            },
                 plugins: [
+                    Taucharts.api.plugins.get('quick-filter')(['commit_date']),
                     Taucharts.api.plugins.get('tooltip')(),
                     Taucharts.api.plugins.get('legend')()
                 ]
-            }).renderTo('#chart');
+            };
+
+            chart = new Taucharts.Chart(chartConfig);
+            chart.renderTo("#chart");
+
+            let xSelect = $("#x-axis");
+            let ySelect = $("#y-axis");
+
+            for (let prop in chartData[0]) {
+                xSelect.append($('<option>', {value: prop, text: prop}));
+                ySelect.append($('<option>', {value: prop, text: prop}));
+            }
+
+            xSelect.val('commit_date');
+            ySelect.val('numberOfFiles');
+
+            xSelect.on('change', e => {
+                chartConfig.x = xSelect.val();
+                updateChart();
+            });
+
+            ySelect.on('change', e => {
+                chartConfig.y = ySelect.val();
+                updateChart();
+            });
         })
         .catch(e => {
             console.log("Error: ", e);
@@ -73,5 +106,4 @@ function submitForm(event) {
             $("#error").html(e.responseText).show();
             $("#form").show();
         });
-    console.log('run');
 }
